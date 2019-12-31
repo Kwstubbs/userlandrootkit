a8ed4479a36067b58a7b43db14b56bd7d3c17664
Kwstubbs <kstubbin@ucsd.edu>
2019-12-30 16:43:03 -0800

    start

---
+++ userlandrootkit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <dlfcn.h>
+#include <dirent.h>
+#include <arpa/inet.h>
+
+
+void ipv6
